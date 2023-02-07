35605512bb290ab817cf759b94b14703d876a908
JPedroc17 <jpccarvalho2210@gmail.com>
2023-02-07 12:01:30 -0300

    Create map close check functions

--- source/validations.c
+++ source/validations.c
@@ -128,3 +128,70 @@ int	is_valid_direction(t_map *map)
 				REDN, 0));
 	return (1);
 }
+
+int	above_and_below(t_map **map, int i, int j, int x)
+{
+	int og;
+	char	*tmp;
+
+	og = j;
+	if (i >= array_length((*map)->map) || i <= 0)
+		return (0);
+	tmp = (*map)->map[i];
+	while (j < x)
+	{
+		if (tmp[j] != '1' && tmp[j] != ' ')
+			return (0);
+		j++;
+	}
+	if (j > og)
+		return (j);
+	return (1);
+}
+
+int	check_surroundings(t_map **map, int i, int j)
+{
+	int x;
+	int ret;
+	int ret2;
+
+	x = j;
+	while ((*map)->map[i][x] == ' ')
+		x++;
+	ret = above_and_below(map, i + 1, j, x);
+	ret2 = above_and_below(map, i - 1, j, x);
+	if (ret == 0 || ret2 == 0)
+		return (0);
+	if (x > 1)
+		return (x);
+	return (j);
+}
+
+int last_map_check(t_map **map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while ((*map)->map[++i])
+	{
+		j = ignore_spaces((*map)->map[i], 0);
+		while ((*map)->map[i][++j] != '\n' && (*map)->map[i][j])
+		{
+			if (ft_strchr("NWSE", (*map)->map[i][j]))
+			{
+				(*map)->p_pos[0] = (float)j;
+				(*map)->p_pos[1] = (float)i;
+				(*map)->start_orientation = (*map)->map[i][j];
+				(*map)->map[i][j] = '0';
+			}
+			if ((*map)->map[i][j] == ' ')
+			{
+				j = check_surroundings(map, i, j);
+				if (j == 0)
+					return (print_error("Map isn't closed by wall\n", REDN, 0));
+			}
+		}
+	}
+	return (1);
+}
